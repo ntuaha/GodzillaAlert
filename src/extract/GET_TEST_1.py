104e2eeb47cbf5d9f8508cbade52fba117235203
Cheng-Yu Lin <ntuaha@gmail.com>
2015-01-02 02:10:48 +0800

    增加固定執行程式

--- src/extract/GET_TEST_1.py
+++ src/extract/GET_TEST_1.py
@@ -56,15 +56,16 @@ class DB:
     return result
 
 class EVENT_STATUS:
-  stop = 0
-  running = 1
-  monitoring = 2
+  STOP = 0
+  RUNNING = 1
+  WATCHING = 2
 
 
-class GET_TEST:
+class GET_TEST(EVENT_STATUS):
   def __init__(self,filepath):
     self.db = DB(filepath)
     self.db.startDB()
+
 
   def close(self):
     self.db.endDB()
@@ -72,9 +73,7 @@ class GET_TEST:
 
 
   def extractData(self,tds):
-    '''
 
-    '''
     #no of row, but its useless
     no = tds[0]
     #event type: 道路施工
@@ -86,10 +85,10 @@ class GET_TEST:
     #路況說明
     description = tds[3].text
     #路況說明: 持續, 排除, 後續
-    event_status = EVENT_STATUS.running
+    event_status = self.RUNNING
     for status in tds[3].xpath('./font/text()'):
       if status =="排除":
-        event_status = EVENT_STATUS.stop
+        event_status = self.STOP
         break
 
     #事件起始時間
@@ -98,57 +97,49 @@ class GET_TEST:
     else:
       start_dt = '1970-01-01'
 
-    return [no,event_type,event_prodiver,event_name,event_status,start_dt,description]
+    return [no,event_type,event_provider,event_name,event_status,start_dt,description]
 
+  #加入Log
   def addLog(self,event_id,status):
     sql = "INSERT INTO event_log (event_id,status,log_dt) VALUES (%d,%d,now());"%(event_id,status)
     self.db.execute(sql)
 
+  #更新事件狀態
   def updateEvent(self,event_id,status,event_source):
     sql = "UPDATE event SET update_dt=now() where event_id = %d"%event_id
     self.db.execute(sql)
     self.addLog(event_id,status)
 
+
+  #結束事件
   def closeEvent(self,event_id,status,event_source):
     sql = "UPDATE event SET update_dt=now(), status=%d,end_dt=now() WHERE event_id=%d "%(event_status,event_id)
     self.db.execute(sql)
-    self.addLog(event_id,status,event_source)
+    self.addLog(event_id,status)
 
+  #建立新事件
   def createEvent(self,event_name,event_type,start_dt,event_source,event_provider,description):
-    sql = "INSERT INTO event (name,type,start_dt,update_dt,status,source,provider,description) VALUES ('%s','%s','%s',now(),%d,'%s','%s','%s');"%(event_name,event_type,start_dt,event_source,Event_Status.running,event_provider,description)
+    sql = "INSERT INTO event (name,type,start_dt,update_dt,status,source,provider,description) VALUES ('%s','%s','%s',now(),%d,'%s','%s','%s');"%(event_name,event_type,start_dt,self.RUNNING,event_source,event_provider,description)
     self.db.execute(sql)
-    event_id = self.getEventId(data_dt,description)
-    self.addLog(event_id,"新增活動",event_source)
+    event_id = self.getEventId(event_source,start_dt,description)
+    self.addLog(event_id,self.RUNNING)
     return event_id
 
+  #強迫事件結束
   def forceCloseEvent(self,id_list,source):
+    print id_list
     ids = ",".join(map(str,id_list))
-    sql = "SELECT event_id FROM event WHERE event_id not in (%s) and source='%s' and status=%d"%(ids,source,EVENT_STATUS.running)
+    sql = "SELECT event_id FROM event WHERE event_id not in (%s) and source='%s' and status=%d"%(ids,source,self.RUNNING)
     result = self.db.select(sql)
     close_ids = [r[0] for r in result]
     print close_ids
-    sql = "UPDATE event SET status=%d where event_id in (%s)"%(EVENT_STATUS.stop,",".join(map(str,close_ids)))
-    self.db.execute(sql)
+    if len(close_ids)>0:
+      sql = "UPDATE event SET status=%d where event_id in (%s)"%(self.STOP,",".join(map(str,close_ids)))
+      self.db.execute(sql)
+      for i in close_ids:
+        self.addLog(close_ids[i],self.STOP)
 
   def getDATA(self):
-    '''
-  event_id bigserial,
-  start_dt timestamp,
-  end_dt timestamp,
-  update_dt timestamp,
-  predict_time timestamp,
-  name varchar,
-  type varchar,
-  status varchar,
-  source varchar,
-  provider varchar,
-  position_desc varchar,
-  lat real,
-  lng real,
-  alt real,
-  address varchar,
-  description varchar,
-    '''
 
     #download data from 警廣
     f = os.path.dirname(__file__)+'/data.dat'
@@ -165,20 +156,21 @@ class GET_TEST:
     for i in range(length):
       #Process on each row
       tds = rows[i].xpath('.//td')
-      [no,event_type,event_prodiver,event_name,event_status,start_dt,description] = self.extractData(tds)
-      event_id = self.getEventId(event_source,data_dt,description,event_source)
+      [no,event_type,event_provider,event_name,event_status,start_dt,description] = self.extractData(tds)
+      event_id = self.getEventId(event_source,start_dt,description)
 
       if event_id > 0 :
-        if event_status==EVENT_STATUS.stop:
+        if event_status==self.STOP:
           self.closeEvent(event_id,event_status,event_source)
         else:
           self.updateEvent(event_id,event_status,event_source)
         id_list.append(event_id)
       else:
-          id = self.createEvent(self,event_name,event_type,start_dt,event_source,event_provider,description)
+          id = self.createEvent(event_name,event_type,start_dt,event_source,event_provider,description)
           id_list.append(id)
     #清掉其他沒被更新，但目前還在進行中的事件
-    self.forceCloseEvent(id_list)
+
+    self.forceCloseEvent(id_list,event_source)
     #結束這個檔案的處理
     os.system('rm %s'%f)
 
@@ -198,6 +190,7 @@ class GET_TEST:
 
 
 if __name__ == '__main__':
+  print EVENT_STATUS.RUNNING
   print os.path.dirname(__file__)+'/../../link.info'
   ahaDB = GET_TEST(os.path.dirname(__file__)+'/../../link.info')
   ahaDB.getDATA()
