22bb03c0e49f5f6ef1d0e063fdb94b5a7cb13ae3
Cheng-Yu Lin <ntuaha@gmail.com>
2015-01-03 18:48:21 +0800

    #7 補上缺加上的資料

--- sql/event.sql
+++ sql/event.sql
@@ -1,30 +1,19 @@
 drop table event;
-drop table event_log;
 
 create table event(
   event_id bigserial,
-  start_dt timestamp with time zone ,
+  title varchar,
+  start_dt timestamp with time zone,
   end_dt timestamp with time zone,
   update_dt timestamp with time zone,
   predict_time timestamp with time zone,
-  name varchar,
   type varchar,
+  description varchar,
   status integer,
-  source varchar,
-  provider varchar,
-  position_desc varchar,
   lat real,
   lng real,
   alt real,
   address varchar,
-  description varchar,
-  link varchar,
-  primary key (event_id)
-);
-
-create table event_log(
-  event_id bigint,
-  status varchar,
-  log_dt timestamp with time zone
+  primary key(event_id)
 );
 
